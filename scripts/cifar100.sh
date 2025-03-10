da9d95523214af83d7bdc6af06e98eb0118b0bf3
Jack Li <zl502@cs.rutgers.edu>
2025-03-10 13:55:18 -0400

    fixing bash script for cifar100

--- scripts/cifar100.sh
+++ scripts/cifar100.sh
@@ -3,11 +3,11 @@ cd ..
 CUDA_VISIBLE_DEVICES=0 python main.py \
 --exp_name '/exp' \
 --dataset 'cifar100' \
---total_nc 4 \
---fg_nc 2 \
---task_num 1 \
+--total_nc 100 \
+--fg_nc 10 \
+--task_num 9 \
 --bs 256 \
---epochs 1 \
+--epochs 50 \
 --seed 77 \
 --pretrain_method '1k' \
 --loss 'protocon' \
